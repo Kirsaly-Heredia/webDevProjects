edd00b57f69c23194fb286cd17190b191fbce8a2
Kirsaly Heredia <kh1943@nyu.edu>
2015-09-29 09:55:35 -0400

    sept 29
    Media and JS HW

---
+++ media_queries/scripts/scripts.js
@@ -0,0 +1,62 @@
+
+//Chris Brown Album
+var cButton = document.querySelector('#cButton');
+var chrisOL = document.querySelector('#chrisOL');
+//Exclusive Album
+var eButton = document.querySelector('#eButton');
+var exclusiveOL = document.querySelector('#exclusiveOL');
+//Graffiti Album
+var gButton = document.querySelector('#gButton');
+var graffitiOL = document.querySelector('#graffitiOL');
+//FAME Album
+var fButton = document.querySelector('#fButton');
+var fameOL = document.querySelector('#fameOL');
+//Fortune Album
+var foButton = document.querySelector('#foButton');
+var fortuneOL = document.querySelector('#fortuneOL');
+//X Album
+var xButton = document.querySelector('#xButton');
+var xOL = document.querySelector('#xOL');
+
+//Chris Brown Album
+cButton.onmouseover = function() {
+	chrisOL.classList.add('show');
+} 
+cButton.onmouseout = function() {
+	chrisOL.classList.remove('show');
+} 
+//Exclusive Album
+eButton.onmouseover = function() {
+	exclusiveOL.classList.add('show');
+} 
+eButton.onmouseout = function() {
+	exclusiveOL.classList.remove('show');
+} 
+//Graffiti Album
+gButton.onmouseover = function() {
+	graffitiOL.classList.add('show');
+} 
+gButton.onmouseout = function() {
+	graffitiOL.classList.remove('show');
+} 
+//FAME Album
+fButton.onmouseover = function() {
+	fameOL.classList.add('show');
+} 
+fButton.onmouseout = function() {
+	fameOL.classList.remove('show');
+} 
+//Fortune Album
+foButton.onmouseover = function() {
+	fortuneOL.classList.add('show');
+} 
+foButton.onmouseout = function() {
+	fortuneOL.classList.remove('show');
+} 
+//X Album
+xButton.onmouseover = function() {
+	xOL.classList.add('show');
+} 
+xButton.onmouseout = function() {
+	xOL.classList.remove('show');
+} 
